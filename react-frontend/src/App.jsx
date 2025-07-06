4e73e6f3485ec68c63d52a4c8f0b59caa66efc07
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2025-07-06 22:45:39 +0100

    Completed MVP features to create a heading and body for a new entry

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,105 +1,62 @@
 import { useState } from 'react'
 
-const Button = ({ onClick, text }) => {
 
-  //Needs to increase the counter for whether it is good, bad or neutral
-  //When the button is clicked, pass in variable to manipulate
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+const Entry = ({ heading, body }) => {
 
-const Statistics = ({ good, neutral, bad }) => {
-
-  console.log('these are the props', { good })
-
-  const total = (good + neutral + bad)
-  const average = total / 3
-  const positive = (good / total) * 100
-
-  if (total === 0) {
+  if (heading || body) {
     return (
-      <div>
-        <p>no feedback given...</p>
-      </div>
+      <>
+        <h2>
+          {heading}
+        </h2>
+        <p>
+          {body}
+        </p>
+      </>
     )
   }
 
   return (
-    <div>
-      <table>
-        <tbody>
-          <StatisticLine text="Good" value={good} />
-          <StatisticLine text="Neutral" value={neutral} />
-          <StatisticLine text="Bad" value={bad} />
-          <StatisticLine text="Total" value={total} />
-          <StatisticLine text="Average" value={average} />
-          <StatisticLine text="Positive" value={positive} />
-        </tbody>
-      </table>
-    </div>
+    <>
+      <p> Awaiting input...</p>
+    </>
   )
 }
 
-const StatisticLine = ({ text, value }) => {
-
-  if (text === "Positive") {
-    return (
-
-      <tr>
-        <td>{text}</td>
-        <td>{value} %</td>
-      </tr>
-    )
-  }
-
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-
-}
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [heading, setHeading] = useState("")
+  const [body, setBody] = useState("")
+  const [submittedHeading, setSubmittedHeading] = useState("")
+  const [submittedBody, setSubmittedBody] = useState("")
 
-  const increaseGood = () => {
+  console.log("Heading --> ", heading)
+  console.log("Body-> ", body)
 
-    console.log('increasing, value before', { good })
-    setGood(good + 1)
-  }
-
-  const increaseNeutral = () => {
-
-    console.log('increasing, value before', { neutral })
-    setNeutral(neutral + 1)
-  }
-
-  const increaseBad = () => {
-
-    console.log('increasing, value before', { bad })
-    setBad(bad + 1)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSubmittedHeading(heading)
+    setSubmittedBody(body)
   }
 
   return (
+
     <div>
-      <h1>give feedback</h1>
-      <Button onClick={increaseGood} text="good" />
-      <Button onClick={increaseNeutral} text="neutral" />
-      <Button onClick={increaseBad} text="bad" />
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>Create a page</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={heading} placeholder="heading" onChange={(e) => setHeading(e.target.value)} />
+        <br />
+        <input value={body} placeholder="body" onChange={(e) => setBody(e.target.value)} />
+        <br />
+        <button type="submit">SUBMIT</button>
+      </form>
+
+      <Entry heading={submittedHeading} body={submittedBody} />
     </div>
+
   )
+
 }
-
-
 
 export default App
