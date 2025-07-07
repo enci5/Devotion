cda81ab38b718019d870138f6a749c78c698595a
Enci <eh00938@surrey.ac.uk>
2025-07-07 20:13:03 +0100

    added post request

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,25 @@ app.get('/api/notes', (req, res)=>{
     })
 })
 
+app.post('/api/notes', async (req,res) =>{
+    const title = req.body.title
+    const content = req.body.content
+    
+    const note = new Note({
+        title:title,
+        content:content
+    })
+    try {
+        await note.save()
+        res.status(200).json(`${title} is added to the database`)
+        console.log(`Added ${title} to database`)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({error: 'Bad Request'})
+    }
+    
+})
+
 const PORT =3001
 app.listen(PORT, ()=>{
     console.log(`Server runnning on port ${PORT}`)
