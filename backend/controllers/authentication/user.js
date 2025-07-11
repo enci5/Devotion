a95bc5a54fcbc0e12909319d55d5358af245bea3
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2025-07-11 02:10:30 +0100

    Added comments of things to change

--- backend/controllers/authentication/user.js
+++ backend/controllers/authentication/user.js
@@ -9,10 +9,17 @@ const jwt = require('jsonwebtoken')
 
 userRouter.post('/login', async (req, res) => {
 
+
+    //hard corded username and password
+    // Take the current username and password otherwise
+    // maybe like const user = await User.findOne({req.body.username})
     if (req.body.username === 'test' && req.body.password === 'pass') {
 
         const payload = {
+            //Again this is just hardcoded with a user fill this in with the current
+            // Logged in user
             username: req.body.username
+            //userid: req.body.user_id
         }
 
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '10h' })
